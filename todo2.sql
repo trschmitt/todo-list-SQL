3fc85ac9b70ccb3eb8fce4b0231c615ff31bbb84
Tristan Schmitt <trschmitt50@live.com>
2017-08-21 11:04:21 -0400

    Resubmission code final

--- todo2.sql
+++ todo2.sql
@@ -18,7 +18,7 @@ INSERT INTO todos(title, details, priority, created_at, completed_at) VALUES
     ('Start Ozark', null, 5, now(), null),
     ('Play Overwatch', 'one of the best games known to man', 1, now(), now()),
     ('Play Destiny', 'yep loved this one', 4, now(), now()),
-    ('Watch all HP movies','done this SO MANY TIMES... bout time for a rematch', 1, now(), now()),
+    ('Watch all HP movies','done this SO MANY TIMES... bout time for a rematch', 1, '2017-06-20 9:30', now()),
     ('Stuff', 'stuff goes here', 1, now(), null),
     ('Finish this class', 'almost there', 1, now(), null),
     ('Catch up on facebook', null, 4, now(), null),
@@ -26,10 +26,10 @@ INSERT INTO todos(title, details, priority, created_at, completed_at) VALUES
     ('Practice Code', 'do more of all of the things', 4, now(), now()),
     ('Go for a run', null, 4, now(), null),
     ('Start another series', null, 5, now(), null),
-    ('Go for a run', null, 1, now(), null),
+    ('Go for a run', null, 1, '2017-06-20 9:30', null),
     ('Go for a run', null, 2, now(), now()),
     ('Go for a run', null, 3, now(), null),
-    ('Go for a run', null, 5, now(), now());
+    ('Go for a run', null, 5, '2017-08-21 11:00', now());
 
 --Select all that are not complete and are priority of 3
 SELECT * FROM todos WHERE completed_at IS NULL AND priority = 3;
@@ -37,35 +37,17 @@ SELECT * FROM todos WHERE completed_at IS NULL AND priority = 3;
 --Statement to find the number of incomplete todos by priority
 SELECT COUNT(completed_at IS NULL)
 FROM todos
-WHERE priority = 1;
-
-SELECT COUNT(completed_at IS NULL)
-FROM todos
-WHERE priority = 2;
-
-SELECT COUNT(completed_at IS NULL)
-FROM todos
-WHERE priority = 3;
-
-SELECT COUNT(completed_at IS NULL)
-FROM todos
-WHERE priority = 4;
-
-SELECT COUNT(completed_at IS NULL)
-FROM todos
-WHERE priority = 5;
-
+GROUP BY priority;
 
 --Statement to find the number of todos by priority created in the last 30 days.
 SELECT COUNT(*)
 FROM todos
-WHERE priority = 1 AND created_at > now()::date - 30;
+WHERE created_at > now()::date - 30
+GROUP BY priority;
 
 -- Statement to find the next todo you should work on. This is the todo with the highest priority that was created first.
 
 SELECT title, details, priority, created_at
 FROM todos
 WHERE priority = 1
-AND created_at =
-    (SELECT MAX(created_at)
-     FROM todos);
+AND created_at >= now()::date;
